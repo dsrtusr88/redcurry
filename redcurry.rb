9f3e5c78aa75e65788dc91d40331d46eacad2382
_mclovin <perun@nolyfe>
2021-07-09 20:35:46 +0100

    Account for OPS using torrentId instead of torrentid as key in API JSON response.

--- redcurry.rb
+++ redcurry.rb
@@ -388,7 +388,8 @@ def curry(sourceAPI, targetAPI, target_authkey, target_passkey, torrent_id, sour
     if !$TARGET_API_KEY.nil?
       upload_response = targetAPI.post("upload", target_payload) 
       upload_response = upload_response.kind_of?(Array) ? upload_response[0] : upload_response
-      new_torrent_url = "#{$TARGET_WEB_URL}/torrents.php?torrentid=#{upload_response['torrentId']}"
+      torrentKey = TARGET_ANNOUNCE_HOST.include?("opsfet") ? "torrentId" : "torrentid"
+      new_torrent_url = "#{$TARGET_WEB_URL}/torrents.php?torrentid=#{upload_response[torrentKey]}"
     else
       new_torrent_url = "#{$TARGET_WEB_URL}/#{targetAPI.upload(target_payload)}"
     end
